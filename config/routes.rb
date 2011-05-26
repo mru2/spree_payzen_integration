cf6903e125228323e56f89cf0317354b9834b7cb
David Ruyer <david.ruyer@gmail.com>
2011-05-26 17:55:17 +0200

    JS fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # Add your extension routes here
-
   match 'payment/payzen/confirm' => 'checkout#payzen', :as => :payzen
   match 'payment/payzen/back' => 'checkout#payzen_back', :as => :payzen_back
 end
